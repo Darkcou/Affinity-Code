5281a3ac00c3ecd3b98f51432917d42d902b79d6
Yann Christophe Maertens <iroshinidako@gmail.com>
2020-05-18 16:41:05 +0200

    add lessons on LesonsView, create a QuestionView

---
+++ QuestionView.swift
@@ -0,0 +1,21 @@
+//
+//  QuestionView.swift
+//  AffinityCode
+//
+//  Created by Yann Christophe Maertens on 18/05/2020.
+//  Copyright © 2020 Benjamin Gronier. All rights reserved.
+//
+
+import SwiftUI
+
+struct QuestionView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct QuestionView_Previews: PreviewProvider {
+    static var previews: some View {
+        QuestionView()
+    }
+}
